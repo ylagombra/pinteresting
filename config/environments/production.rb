7bb2168b8b668baf9727f52de77e3d53759b4557
Yardley Lagombra <ylagombra@yahoo.com>
2014-11-11 15:10:21 -0500

    Implemented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+   # Required for Heroku
+   #note to set this to actual host name
+    config.action_mailer.default_url_options = { :host => 'http://shadow-code.herokuapp.com/'}
 end
